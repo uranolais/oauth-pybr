d431260a5f749b2fec74be963243ab58d7a27a77
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-10-04 00:26:10 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'pybr',
 ]
 
